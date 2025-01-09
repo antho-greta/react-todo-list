a9e92aa1a7b478eb2aaa8cdc3b444ce28a58f9b4
Anthony Mendes <anthony@iMac-de-Anthony.local>
2025-01-09 11:21:46 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Lego from "./components/lego.jsx";
+
+
+function App() {
+
+  return (
+    <>
+      <p>coucou</p>
+        <Lego/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/lego.jsx
@@ -0,0 +1,7 @@
+function Lego() {
+    return <div className={'bg-red-500 h-16 w-32 items-center text-center justify-center'}>
+        <h1>LEGO</h1>
+    </div>
+}
+
+export default Lego;
